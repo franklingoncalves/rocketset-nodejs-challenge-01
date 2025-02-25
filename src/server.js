a661ed9e1ddc15c15f0c017c3641de3c30d75f4e
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-02-25 18:22:11 -0300

    feat: initialize project with npm, set up server and dev script

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify({ message: "Hello World" }));
+  }
+});
+
+server.listen(3333, () => {
+  console.log("Server is running");
+});

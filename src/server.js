725cb08d19aa4411ca5bd13b6a8a7efa10d8068e
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-03-05 18:23:34 -0300

    feat: alter `server.js` to handle task creation route

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import http from "node:http";
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/") {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify({ message: "Hello World" }));
+import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+  await json(req, res);
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+  if (route) {
+    const routeParams = req.url.match(route.path);
+    const { query, ...params } = routeParams.groups;
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+    return route.handler(req, res);
   }
+  return res.writeHead(404).end();
 });
 
-server.listen(3333, () => {
-  console.log("🚀 Server is running");
+const port = process.env.PORT || 3333;
+
+server.listen(port, () => {
+  console.log(`🚀 Server is running on Port ${port}`);
 });

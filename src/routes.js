ba62d11ad7b1e8fb5efaa77353b79f6d4d5180ee
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-03-11 12:56:12 -0300

    feat: update tasks by id

--- src/routes.js
+++ src/routes.js
@@ -51,4 +51,34 @@ export const routes = [
       return res.end(JSON.stringify(tasks));
     },
   },
+  {
+    method: "PUT",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description } = req.body;
+
+      if (!title && !description) {
+        return res
+          .writeHead(400)
+          .end(
+            JSON.stringify({ message: "title or description are required" })
+          );
+      }
+
+      const [task] = database.select("tasks", { id });
+
+      if (!task) {
+        return res.writeHead(404).end();
+      }
+
+      database.update("tasks", id, {
+        title: title ?? task.title,
+        description: description ?? task.description,
+        updated_at: new Date(),
+      });
+
+      return res.writeHead(204).end();
+    },
+  },
 ];
